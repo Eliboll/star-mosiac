29c8415dad66b07d572fdc8aff2bae4c62292eef
Eliboll <eetes77@gmail.com>
2024-11-27 14:42:58 -0600

    keypoint caching, save a lot of computation time every execution

--- star_mosaic.py
+++ star_mosaic.py
@@ -5,8 +5,10 @@ from match_keypoints import find_matching_points_parallel
 import time
 
 class star_mosaic:
-    def __init__(self):
+    def __init__(self, CACHE=False):
         self.images: list[mosaic_image] = []
+        self.__matching_points = []
+        self.__cache = CACHE
         
     def add_raw_image(self, image:np.ndarray) -> None:
         self.images.append(mosaic_image(image))
@@ -23,6 +25,13 @@ class star_mosaic:
             self.add_mosaic_image(image)
             
     def match_keypoints(self):
+        if self.__cache:
+            import os.path
+            if os.path.exists(".matches.cache"):
+                self.__matching_points = open_cache(".matches.cache")
+        
+        if self.__matching_points:
+            return self.__matching_points
         img1_map = self.images[0].get_relationships()
         img2_map = self.images[1].get_relationships()
             
@@ -32,5 +41,28 @@ class star_mosaic:
         print(f"{end - start:.2f} second elapsed")
         
         return matches[:100]
+        if self.__cache:
+            write_cache(".matches.cache", self.__matching_points)
+        return self.__matching_points
         
-        
+        def open_cache(filename):
+    cache = []
+    with open(filename, "r") as fp:
+        for line in fp.readlines():
+            parts = line.split(',')
+            c1 = (int(parts[0]), int(parts[1]))
+            c2 = (int(parts[2]), int(parts[3]))
+            cost = float(parts[4])
+            ratio = float(parts[5])
+            cache.append((c1,c2,cost,ratio))
+    return cache
+            
+            
+def write_cache(filename, matches):
+    with open(filename,"w") as fp:
+        for match in matches:
+            fp.write(f"{match[0][0]},{match[0][1]},{match[1][0]},{match[1][1]},{match[2]},{match[3]}\n")
+            
+if __name__ == "__main__":
+    from main import main
+    main()
